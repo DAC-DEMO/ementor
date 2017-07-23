46adc0db41c9b1f6b46da2356741fb0d6ee34502
santosh <santosh.ece06@gmail.com>
2017-07-23 10:36:40 +0530

    new view added.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,6 +25,12 @@ app.config(function($routeProvider) {
         "controller": "page2"
     });
 
+
+    $routeProvider.when("/page3", {
+        "templateUrl": "./src/views/page3.html",
+        "controller": "page3"
+    });
+
 });
 
 
---
+++ src/assets/js/page3.js
@@ -0,0 +1,8 @@
+var app = angular.module("app");
+
+
+app.controller("page3", function($scope) {
+    console.log("Page3");
+
+
+});
